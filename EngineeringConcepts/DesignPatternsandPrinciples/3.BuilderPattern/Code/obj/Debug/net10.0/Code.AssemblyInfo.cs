865f4ea9ea7db296c06bb47ed4131077a0a7e68a
Nirnay Ghosh <ghoshnirnay2@gmail.com>
2026-06-20 20:31:36 +0530

    modified

--- EngineeringConcepts/DesignPatternsandPrinciples/3.BuilderPattern/Code/obj/Debug/net10.0/Code.AssemblyInfo.cs
+++ EngineeringConcepts/DesignPatternsandPrinciples/3.BuilderPattern/Code/obj/Debug/net10.0/Code.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Code")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1629e9ddefd706d5ffe2da2b16f1f06bfa6367ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7c7dfb4f17c91b29d7a97ef3dad344596b0270b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Code")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Code")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
